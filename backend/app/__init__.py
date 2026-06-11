618663611bbb6c95432eb5cbdfd0bbbdb5f1cce4
Ghaithkrid <kridghaith@gmail.com>
2026-06-11 14:21:37 +0100

    Connect React frontend to Flask API

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 db=SQLAlchemy()
 def create_app():
     app=Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///finance.db"
     db.init_app(app)
+    CORS(app)
     with app.app_context():
         from app.models.transactions import Transaction
         db.create_all()
